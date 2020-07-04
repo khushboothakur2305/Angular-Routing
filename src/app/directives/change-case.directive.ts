279524463fbcf506bfb9dccec3aa27496ab93cd9
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-07-05 00:27:44 +0530

    update

--- src/app/directives/change-case.directive.ts
+++ src/app/directives/change-case.directive.ts
@@ -1,14 +1,17 @@
-import { Directive, ElementRef,OnInit } from '@angular/core';
+import { Directive, ElementRef,OnInit, HostListener, Renderer2, Input } from '@angular/core';
 
 @Directive({
   selector: '[appChangeCase]'
 })
 export class ChangeCaseDirective {
+  @Input('appChangeCase') case:string;
+  @HostListener('blur') onblur(){
+    let changedValue:string=this.case==='upper'? this.elRef.nativeElement.value.toUpperCase():
+    this.elRef.nativeElement.value.toLowerCase();
+    this.renderer.setProperty(this.elRef.nativeElement,'value',changedValue);
+  }
+  constructor(private elRef:ElementRef , private renderer:Renderer2) {}
 
-  constructor(private elref:ElementRef ) {}
-  ngOnInit(){
-console.log(this.elref.nativeElement.value);
-
-
+    ngOnInit(){
   }
 }
